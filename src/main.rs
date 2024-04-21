8653dd4bfb3d417b04edbcfdf1179db97f10da89
Adam Perkowski <adas1per@protonmail.com>
2024-04-21 16:30:11 +0200

    huh

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::fmt::Write;
+use std::{ fmt::Write, time::Duration, thread };
 
 mod res;
 
 pub fn main() {
     let mut screen = res::screen::DebugScreen::new();
+
     writeln!(screen, "Welcome to Para Wodna Sync for PlayStation Vita!").ok();
+
+    thread::sleep(Duration::from_secs(5));
 }
