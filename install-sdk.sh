6a8bb1eb4b702098df6668fdbafeedaa61a2928b
Adam Perkowski <adas1per@protonmail.com>
2024-07-17 09:26:42 +0200

    i dont know...

--- install-sdk.sh
+++ install-sdk.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get install make git-core cmake python2 python3 -y
 
-export VITASDK=/usr/local/vitasdk
-export PATH=$VITASDK/bin:$PATH
+# export VITASDK=/usr/local/vitasdk
+# export PATH=$VITASDK/bin:$PATH
 
 git clone https://github.com/vitasdk/vdpm
 cd vdpm
